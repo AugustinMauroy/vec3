bda6c4036266b807d5cc964ebe22351b9da1e6ff
Augustin Mauroy <97875033+AugustinMauroy@users.noreply.github.com>
2025-05-05 10:03:07 +0200

    fix: code format

--- bench/math-util.bench.js
+++ bench/math-util.bench.js
@@ -1,23 +1,22 @@
-import { Suite } from 'bench-node';
-import { euclideanMod } from '../dist/math-util.js';
+import { Suite } from "bench-node";
+import { euclideanMod } from "../dist/math-util.js";
 
 const suite = new Suite();
 
-suite.add('euclideanMod positive numbers', () => {
-    euclideanMod(12345, 67);
+suite.add("euclideanMod positive numbers", () => {
+	euclideanMod(12345, 67);
 });
 
-suite.add('euclideanMod negative numerator', () => {
-    euclideanMod(-12345, 67);
+suite.add("euclideanMod negative numerator", () => {
+	euclideanMod(-12345, 67);
 });
 
-suite.add('euclideanMod negative denominator', () => {
-    euclideanMod(12345, -67);
+suite.add("euclideanMod negative denominator", () => {
+	euclideanMod(12345, -67);
 });
 
-suite.add('euclideanMod both negative', () => {
-    euclideanMod(-12345, -67);
+suite.add("euclideanMod both negative", () => {
+	euclideanMod(-12345, -67);
 });
-
 
 suite.run();
