168eddea4980bdfed27f81e34aec9b960347336d
Augustin Mauroy <97875033+AugustinMauroy@users.noreply.github.com>
2025-06-06 10:27:23 +0200

    refracto: archi structure

--- src/mod.test.ts
+++ src/mod.test.ts
@@ -1,6 +1,6 @@
 import assert from "node:assert/strict";
 import { describe, it } from "node:test";
-import { Vec3, v } from "../src/mod.ts";
+import { Vec3, v } from "./mod.ts";
 
 describe("v()", { concurrency: true }, () => {
 	it("no args", () => {
